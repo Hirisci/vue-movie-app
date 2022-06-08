ddd89722218ea8a4f2b04a195f6681e2bb6ccc88
Hirisci <www.alan92@icloud.com>
2022-06-08 16:27:30 +0200

    add font-aesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+/* import the fontawesome */
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faUserSecret);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+/* /import the fontawesome */
+
+Vue.config.productionTip = false;
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
